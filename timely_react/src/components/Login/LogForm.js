5fdd55278ec57e5173b340662155fb451ddb0aa8
Dennis Reil <52947123+dennisreil@users.noreply.github.com>
2020-03-06 16:04:29 -0800

    console log displays sumbit fields

--- timely_react/src/components/Login/LogForm.js
+++ timely_react/src/components/Login/LogForm.js
@@ -1,44 +1,101 @@
 import React from 'react';
+
 import '../../Styles/Styles.scss';
 
 class Login extends React.Component {
 
-    constructor(props) {
-    super(props);
-        this.state = {};
-    }
+    constructor() {
+        super();
+        this.state = {
+          email: "",
+          password: "",
+          errors: {}
+        };
+      }
 
-    submitLogin(e) {
-        
-    }
+      //API to back end
+      onLogin = () => {
+        fetch("http://localhost:8080/login", {
+        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+        method: "POST",
+        body: JSON.stringify({ username: this.state.username, password: this.state.password })
+        })
+        .then(res => res.headers.get("authorization"))
+        .then(token => {
+          if (token) {
+            this.setState({ ...this.state, token: token });
+          } else {
+            this.setState({ ...this.state, error: "Unable to login with username and password." });
+          }
+        });
+      }
 
-    render() {
+
+//onChange ties in the form values to the components state
+    onChange = e => {
+        this.setState({ [e.target.id]: e.target.value });
+      };
+
+
+//prevent default stops re-rendering of page
+    onSubmit = e => {
+        e.preventDefault();
+
+
+    const user = {
+            email: this.state.email,
+            password: this.state.password,
+          };
+
+    console.log(user);
+        };
+
+        render() {
+            const { errors } = this.state;
         return  <div className="root-container">
                 <div className="inner-container">
                 <div className="box-header">
-                Login
+                    Register
                 </div>
-    <div className="box">
 
-            <div className="input-group">
-                
-                <label htmlFor="email">Email</label>
-                <input type="text" className="login-input" name="email" placeholder="Email"/>
-            </div>
+        <form onSubmit={this.onSubmit}
+        className="box">
 
-            <div className="input-group">
+<div className="input-group">
+        <label htmlFor="email">Email</label>
 
-                <label htmlFor="password">Password</label>
-                <input type="password" className="login-input" name="password" placeholder="Password"/>
-            </div>
+            <input className="login-input"
+                name="email"
+                placeholder="Email"
+                  onChange={this.onChange}
+                  value={this.state.email}
+                  error={errors.email}
+                  id="email"
+                  type="email"/>
+              </div>
 
-            <button 
+
+    <div className="input-group">
+        <label htmlFor="password">Create Password</label>
+
+            <input className="login-input"
+                name="password"
+                placeholder="Create Password"
+                  onChange={this.onChange}
+                  value={this.state.password}
+                  error={errors.password}
+                  id="password"
+                  type="password"/>
+              </div>
+
+            <button
             className="login-btn"
-            type="button" onCLick={this.submitLogin.bind(this)}>Login</button>
+            type="submit" 
+            onClick={this.props.onLogin}>Login</button>
 
-        </div>
-        </div>
-    </div>
+                </form>
+            </div>
+            </div>
     }
 }
 
--- timely_react/src/components/Register/RegForm.js
+++ timely_react/src/components/Register/RegForm.js
@@ -3,59 +3,126 @@ import React from 'react';
 
 class Register extends React.Component {
 
-    constructor(props) {
-    super(props);
-        this.state = {};
-    }
+    constructor() {
+        super();
+        this.state = {
+          firstName: "",
+          lastName: "",
+          company: "",
+          email: "",
+          password: "",
+          errors: {}
+        };
+      }
 
-    submitRegister(e) {
 
-    }
+//onChange ties in the form values to the components state
+    onChange = e => {
+        this.setState({ [e.target.id]: e.target.value });
+      };
 
-    render() {
+
+//prevent default stops re-rendering of page
+    onSubmit = e => {
+        e.preventDefault();
+
+
+    const newUser = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            company: this.state.company,
+            email: this.state.email,
+            password: this.state.password,
+          };
+
+    console.log(newUser);
+        };
+
+        render() {
+            const { errors } = this.state;
         return  <div className="root-container">
                 <div className="inner-container">
                 <div className="box-header">
                     Register
                 </div>
 
-        <div className="box">
+        <form onSubmit={this.onSubmit}
+        className="box">
 
-        <div className="input-group">
+    <div className="input-group">
+        <label htmlFor="firstName">First Name</label>
 
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" className="login-input" name="firstName" placeholder="FirstName"/>
-        </div>
+            <input className="login-input"
+                name="firstName"
+                placeholder="First Name"
+                  onChange={this.onChange}
+                  value={this.state.firstName}
+                  error={errors.firstName}
+                  id="firstName"
+                  type="text"
+                />
+              </div>
 
-        <div className="input-group">
+    <div className="input-group">
+        <label htmlFor="lastName">Last Name</label>
 
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text" className="login-input" name="lastName" placeholder="LastName"/>
-        </div>
+            <input className="login-input"
+                name="lastName"
+                placeholder="Last Name"
+                  onChange={this.onChange}
+                  value={this.state.lastName}
+                  error={errors.lastName}
+                  id="lastName"
+                  type="text"
+                />
+              </div>
 
-        <div className="input-group">
+    <div className="input-group">
+        <label htmlFor="email">Email</label>
 
-            <label htmlFor="company">Company</label>
-            <input type="text" className="login-input" name="company" placeholder="Company"/>
-        </div>
+            <input className="login-input"
+                name="email"
+                placeholder="Email"
+                  onChange={this.onChange}
+                  value={this.state.email}
+                  error={errors.email}
+                  id="email"
+                  type="email"/>
+              </div>
 
-        <div className="input-group">
 
-            <label htmlFor="email">Email</label>
-            <input type="text" className="login-input" name="email" placeholder="Email"/>
-        </div>
+   <div className="input-group">
+        <label htmlFor="company">Company</label>
 
-        <div className="input-group">
-            
-            <label htmlFor="password">Password</label>
-            <input type="password" className="login-input" name="password" placeholder="Password"/>
-        </div>
+            <input className="login-input"
+                name="company"
+                placeholder="Company"
+                  onChange={this.onChange}
+                  value={this.state.company}
+                  error={errors.company}
+                  id="company"
+                  type="text"/>
+              </div>
+
+
+    <div className="input-group">
+        <label htmlFor="password">Create Password</label>
+
+            <input className="login-input"
+                name="password"
+                placeholder="Create Password"
+                  onChange={this.onChange}
+                  value={this.state.password}
+                  error={errors.password}
+                  id="password"
+                  type="password"/>
+              </div>
 
             <button
             className="login-btn"
-            type="button" onCLick={this.submitRegister.bind(this)}>Register</button>
+            type="submit">Register</button>
 
-                </div>
+                </form>
             </div>
             </div>
     }
