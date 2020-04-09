eaf950e462114e37b6ea39fd110e25d21a5230ce
Rebekah <rjoychin@gmail.com>
2020-04-09 11:21:09 +0530

    added message response in Sales Form which tells user whether their sales entries were successful or duplicates

--- timely_react/src/components/Profile/profile.component.js
+++ timely_react/src/components/Profile/profile.component.js
@@ -20,24 +20,6 @@ import AuthService from "../../Services/auth.service";
             <strong>Welcome {currentUser.username}!</strong>
           </h3>
         </header>
-        <p>
-          <strong>Token:</strong>{" "}
-          {currentUser.accessToken.substring(0, 20)} ...{" "}
-          {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-        </p>
-        <p>
-          <strong>Id:</strong>{" "}
-          {currentUser.id}
-        </p>
-        <p>
-          <strong>Email:</strong>{" "}
-          {currentUser.email}
-        </p>
-        <strong>Authorities:</strong>
-        <ul>
-          {currentUser.roles &&
-            currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-        </ul>
       </div>
     );
   }
